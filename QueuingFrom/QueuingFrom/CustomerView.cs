29d84b10dc6cc0c83e3b36bb7831aab22216c50e
Reddd <107017631+LightWater123@users.noreply.github.com>
2024-09-12 17:02:10 +0800

    Add files via upload

---
+++ QueuingFrom/QueuingFrom/CustomerView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace QueuingFrom
+{
+    public partial class CustomerView : Form
+    {
+        public CustomerView()
+        {
+            InitializeComponent();
+        }
+
+        // update label showing next student in the queue
+        public void update(string nextCustomer)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(nextCustomer))
+                {
+                    label2.Text = nextCustomer;
+                }
+                else
+                {
+                    label2.Text = "Empty.";
+                }
+            }
+            catch (Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
